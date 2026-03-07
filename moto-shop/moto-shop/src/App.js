a131a002d55b169b4f4e2cfb7a45db69cd322132
Wagner Pessoa <wagnerpessoa.dev@gmail.com>
2026-03-07 11:50:52 -0300

    package.json ajustado

--- moto-shop/moto-shop/src/App.js
+++ moto-shop/moto-shop/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import SellSection from './components/SellSection';
 import Inventory from './components/Inventory';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <SellSection />
       <Inventory />
       
       <footer className="footer">
