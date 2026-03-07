7f757b287fd5df64135d7c3c56fbe2ad3c404d03
Wagner Pessoa <wagnerpessoa.dev@gmail.com>
2026-03-07 18:24:05 -0300

    icones contato add

--- moto-shop/moto-shop/src/App.js
+++ moto-shop/moto-shop/src/App.js
@@ -25,10 +25,44 @@ function App() {
             </p>
           </div>
 
-          <div className="footer-links">
+          <div className="footer-contact">
             <h4>contato</h4>
-            <p>(21) 99001-8557</p>
-            <p>@dumotors</p>
+            <div className="contact-list">
+              <a
+                className="contact-item"
+                href="https://wa.me/5521990018557"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="contact-icon" aria-hidden="true">
+                  <svg viewBox="0 0 32 32" role="img" focusable="false">
+                    <path d="M16.02 4C9.39 4 4 9.18 4 15.56c0 2.55.94 4.93 2.53 6.8L4 28l5.8-2.4a12.23 12.23 0 0 0 6.22 1.69c6.63 0 12.02-5.18 12.02-11.56C28.04 9.18 22.65 4 16.02 4Zm0 20.84c-2.01 0-3.96-.55-5.66-1.58l-.41-.25-3.44 1.43 1.48-3.24-.27-.43a9.8 9.8 0 0 1-1.57-5.21c0-5.09 4.4-9.23 9.87-9.23 5.44 0 9.87 4.14 9.87 9.23 0 5.09-4.43 9.28-9.87 9.28Zm5.77-6.93c-.31-.16-1.84-.9-2.13-1.01-.29-.11-.5-.16-.71.16-.21.31-.82 1.01-1 1.22-.18.2-.37.23-.68.08-.31-.16-1.3-.47-2.47-1.51-.91-.79-1.53-1.77-1.71-2.08-.18-.31-.02-.48.14-.64.14-.14.31-.37.47-.55.16-.18.21-.31.31-.52.1-.21.05-.39-.03-.55-.08-.16-.71-1.67-.97-2.29-.26-.62-.53-.54-.71-.55h-.6c-.2 0-.52.08-.79.39-.27.31-1.04 1.02-1.04 2.48 0 1.46 1.07 2.87 1.22 3.07.16.21 2.1 3.28 5.13 4.46.72.28 1.28.45 1.72.57.72.22 1.37.19 1.88.11.57-.08 1.84-.73 2.1-1.43.26-.7.26-1.31.18-1.43-.08-.12-.29-.2-.6-.36Z" />
+                  </svg>
+                </span>
+                <span>(21) 99001-8557</span>
+              </a>
+              <a
+                className="contact-item"
+                href="https://instagram.com/dumotors"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="contact-icon" aria-hidden="true">
+                  <svg viewBox="0 0 32 32" role="img" focusable="false">
+                    <path d="M21 4H11c-3.86 0-7 3.14-7 7v10c0 3.86 3.14 7 7 7h10c3.86 0 7-3.14 7-7V11c0-3.86-3.14-7-7-7Zm5 17c0 2.76-2.24 5-5 5H11c-2.76 0-5-2.24-5-5V11c0-2.76 2.24-5 5-5h10c2.76 0 5 2.24 5 5v10ZM16 10.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11Zm0 9a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Zm5.75-9.88a1.12 1.12 0 1 0 0 2.24 1.12 1.12 0 0 0 0-2.24Z" />
+                  </svg>
+                </span>
+                <span>@dumotors</span>
+              </a>
+              <a className="contact-item" href="mailto:contato@dumotors.com.br">
+                <span className="contact-icon" aria-hidden="true">
+                  <svg viewBox="0 0 32 32" role="img" focusable="false">
+                    <path d="M26 6H6c-1.65 0-3 1.35-3 3v14c0 1.65 1.35 3 3 3h20c1.65 0 3-1.35 3-3V9c0-1.65-1.35-3-3-3Zm0 4.07-9.36 5.85c-.4.25-.9.25-1.3 0L6 10.07V9l10 6.25L26 9v1.07Z" />
+                  </svg>
+                </span>
+                <span>contato@dumotors.com.br</span>
+              </a>
+            </div>
           </div>
 
           <div className="footer-contact">
